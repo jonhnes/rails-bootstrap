896178a06210608df2eb06ca62988d93519557c6
Jonhnes Lopes <jonhnes@gmail.com>
2020-02-24 19:18:14 -0300

    Using new default config

--- rails_configuration.rb
+++ rails_configuration.rb
@@ -4,8 +4,8 @@ class RailsConfiguration
   attr_accessor :app_name, :ruby_version, :rails_version
 
   def initialize
-    @app_name = 'test_app'
+    @app_name = 'rails_bootstrap_app'
     @ruby_version = '2.6.4'
-    @rails_version = '5.2.4'
+    @rails_version = '5.2.1'
   end
 end
